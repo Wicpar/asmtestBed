0201b410834460a4602a98ca0e83ff37533d8e22
Frederic Nieto <f.nieto@groupeonepoint.com>
2016-09-22 16:04:28 +0200

    added linear speed fibonacci-print function

--- main.asm
+++ main.asm
@@ -8,16 +8,14 @@ section .data
 section .text
 CMAIN:
     mov rbp, rsp; for correct debugging
-    mov rdi, 1000000
-.rep:
-    mov rcx, rdi
-    push rdi
-    call _fibonacci
-    ;PRINT_DEC 8, rax
-    ;NEWLINE
-    pop rdi
-    sub rdi, 1
-    jnz .rep
+    mov rcx, 100
+    call _fibonacciPrint
+    xor rax, rax
+    ret
+    
+_putNbr:
+    PRINT_DEC 8, rcx
+    NEWLINE
     ret
     
 _fibonacci:      ; non-recursive so to avoid duplicate calculations
@@ -37,6 +35,47 @@ _fibonacci:      ; non-recursive so to avoid duplicate calculations
 .loopFib:
     add rax, rcx
     add rcx, rax
+    sub rdi, 2
+    jnz .loopFib
+.end:
+    ret
+    
+_fibonacciPrint:      ; non-recursive so to avoid duplicate calculations
+    mov rdi, rcx
+    test rdi, 1    ;TODO find a way to assemble both those tests
+    jnz .rinit
+    test rdi, rdi  ; test for 0 case
+    jz .end
+    xor rax, rax
+    mov rcx, 1    ;using ecx because it is considered volitile
+    jmp .loopFib
+.rinit:           ;aligning to parity so to improve performance
+    mov rcx, 1
+    mov rax, 1
+    sub rdi, 1    ; test for 1 case
+    jz .end
+.loopFib:
+    add rax, rcx
+    
+    push rax
+    push rcx
+    push rdi
+    mov rcx, rax
+    call _putNbr
+    pop rdi
+    pop rcx
+    pop rax
+    
+    add rcx, rax
+    
+    push rax
+    push rcx
+    push rdi
+    call _putNbr
+    pop rdi
+    pop rcx
+    pop rax
+    
     sub rdi, 2
     jnz .loopFib
 .end:
